7f6c875ded0b81fc3027320bf819cc16e7057304
Elliot Rosen <elliotrosen2@gmail.com>
2025-04-22 16:19:53 -0400

    Add blueprint for upload component to app

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-import panda as pd
-import numpy as np
-import xgboost as xgb
-import shap
-import pickle
-import os
+
+from routes.upload import upload_bp
 
 
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(upload_bp, url_prefix='/api/upload')
