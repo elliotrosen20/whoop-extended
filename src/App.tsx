652257870cc5e9c8c2a9107d706b0fdb65d63b8b
Elliot Rosen <elliotrosen2@gmail.com>
2025-04-22 16:21:30 -0400

    Add upload with react Link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
 import './App.css'
-import Upload from './pages/Upload'
+// import Upload from './pages/Upload'
 
 function App() {
   return (
     <>
-      <Upload />
+      {/* <Upload /> */}
+      <h1>Hello app</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="upload">Upload File</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
