be3321eee36cf0ae8b56ee72d485038d269a4eb6
Elliot Rosen <elliotrosen2@gmail.com>
2025-04-24 11:03:19 -0400

    Add blueprint for analyze route

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.upload import upload_bp
+from routes.upload import upload_bp, analyze_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(upload_bp, url_prefix='/api')
+app.register_blueprint(analyze_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
