55d1be342bdc9e0173246ce3553e1641b8343070
Elliot Rosen <elliotrosen2@gmail.com>
2025-04-24 11:04:18 -0400

    Add generating state to upload

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-// import { useNavigate } from 'react-router-dom';
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Upload() {
   const [file, setFile] = useState<File | null>(null)
-  // const navigate = useNavigate();
-  // const [isUploaded, setIsUploaded] = useState<boolean>(false)
+  const navigate = useNavigate();
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
+  const [fileId, setFileId] = useState<string | null>(null);
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async () => {
     try {
@@ -48,10 +52,8 @@ function Upload() {
       }
 
       localStorage.setItem('fileId', data.file_id);
+      setFileId(data.file_id);
       toast.success('File uploaded successfully!');
-
-      // navigate('/dashboard');
-
     } catch (error) {
       console.error('Upload error:', error)
 
@@ -70,16 +72,100 @@ function Upload() {
     setFile(selectedFile);
   }
 
-  // const handleGenerate = () => {
+  const handleGenerate = async () => {
+    const fileId = localStorage.getItem('fileId');
 
-  // }
+    if (!fileId) {
+      toast.error('No file has been uploaded. Please upload a file first.');
+      return;
+    }
+
+    setIsGenerating(true);
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    
+    try {
+      // const response = await fetch(`/api/generate/${fileId}`, {
+      //   method: "POST"
+      // });
+
+      // process response
+
+      // localStorage.setItem('insightsReady', 'true');
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Error:',error);
+    } finally {
+      setIsGenerating(false);
+    }
+  }
+
+  const handleNewUpload = () => {
+    localStorage.removeItem('fileId');
+    setFileId(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+    setFile(null);
+  }
+
+  if (isGenerating){
+    return (
+      <div
+        className="flex flex-col justify-center items-center mt-10"
+      >
+        <ClipLoader
+          color={"#3B82F6"}
+          loading={isGenerating}
+          size={35}
+        />
+        <span>Generating insights...</span>
+      </div>
+    )
+  }
 
   return (
     <div>
       <h1>CSV Upload</h1>
 
-      <input type="file" onChange={handleFileChange}/>
-      <button onClick={handleSubmit}>Upload</button>
+      {fileId ? (
+        <div className="flex flex-col gap-4">
+          <button
+            onClick={handleGenerate}
+            className='mt-10'
+          >
+            Generate insights
+          </button>
+          <h3>or</h3>
+          <button 
+            onClick={handleNewUpload}
+          >
+            Upload a new file
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-10">
+          <input
+            id="file_input"
+            type="file"
+            onChange={handleFileChange}
+            ref={fileInputRef}
+            // className='block'
+            className='block text-sm text-gray-500
+                      file:mr-4 file:py-2 file:px-4
+                      file:rounded file:border-0
+                      file:text-sm file:font-semibold
+                      file:bg-blue-50 file:text-blue-700
+                      hover:file:bg-blue-100 mt-4
+                      border-blue-500 border-2'
+          />
+          <button 
+            onClick={handleSubmit}
+          >
+            Upload
+          </button>
+        </div>
+      )}
 
       <ToastContainer 
         position="top-right"
